b81cbc97a1938c51cc73a1a421a916d15c39b9ee
Przemysław Zawadka <zawadkaprzemek@gmail.com>
2017-10-11 17:20:57 +0200

    poprawka laczenia sie z baza danych

--- conf/data_base.php
+++ conf/data_base.php
@@ -1,16 +1,23 @@
  <?php
 if(($_SERVER['HTTP_HOST']=='localhost')||($_SERVER['SERVER_ADDR']=='127.0.0.1')||($_SERVER['HTTP_HOST']=='10.105.48.188')){
-
-	$servername = $config->getConfig()->db->localhost->host;
-	$username = $config->getConfig()->db->localhost->login;
-	$password = $config->getConfig()->db->localhost->password;
-	$dbase=$config->getConfig()->db->localhost->dbname;
+	$arr=array(
+		"dbname"=>"dyskretna_przygoda",
+	  	"host"=>"localhost",
+	  	"login"=>"root",
+	  	"password"=>"kisiello1989"
+	);
 }else{
-	$servername = $config->getConfig()->db->online->host;
-	$username = $config->getConfig()->db->online->login;
-	$password = $config->getConfig()->db->online->password;
-	$dbase=$config->getConfig()->db->online->dbname;
+	$arr=array(
+		"dbname"=>"www",
+  		"host"=>"localhost",
+	  	"login"=>"www",
+	  	"password"=>"GmSnfAzu"
+	);
 }
+$servername = $arr["host"];
+$username = $arr["login"];
+$password = $arr["password"];
+$dbase = $arr["dbname"];
 $con = new mysqli($servername, $username, $password,$dbase);
 
 if ($con->connect_error) {
